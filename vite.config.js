635cfed92e3e837993d6548d7d6d51c9f65aadd9
Alex <423745+alexgenovese@users.noreply.github.com>
2024-11-04 10:44:53 +0100

    removed

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
                 'resources/js/site.js',
             ],
             refresh: true,
-            publicDirectory: 'storage/app/static',
+            // publicDirectory: 'storage/app/static',
         }),
     ],
 });
